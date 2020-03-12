4486b1f66886915eff7c3616e907dc38668b1588
Daniel Major <danielalvesmajor@outlook.com>
2020-03-12 13:22:54 -0300

    Refatorando em componente

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,9 +2,9 @@ import React from "react"
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
-
 import Helmet from 'react-helmet';
 
+import Card from '../components/Card/Card';
 import Layout from "../components/layout"
 import SEO from "../components/seo";
 
@@ -30,128 +30,22 @@ const Projects = ({ data }) => (
 <section className="section is-size-4-desktop is-size-5-touch">
     <h1 className="title has-text-centered">Projects</h1>
     <p className="has-text-centered">
-        Some of my projects, see more on My 
+        Some of my projects, see more on My
+        {' '}
         <a href="https://github.com/seveshy">Github</a>
     </p>
-
-    <div className="columns">
-        <div className="column">
-            <div className="dm-card">
-                
-            </div>
-        
+        <div className="columns">
+            <div className="column is-half is-offset-one-quarter">
+            <Card 
+                title={"Gitcompare"}
+                subtitle={"Projeto desenvolvido para estudos no bootcamp da Rocketseat"}
+                link={"https://github.com/Seveshy/gitcompare"}
+                tags={["create-react-app", "axios", "styled-components", "MomentJS", "font-awesome"]}
+                image={<Img fixed={data.propject1.childImageSharp.fixed} />}
+                />
+            </div> 
         </div>
-    </div>
-
-    <div className="columns">
-        <div className="column is-half is-offset-one-quarter">
-            <a href="https://github.com/Seveshy/gitcompare">
-                <div className="card">
-                        <div className="card-content">
-                            <div className="media">
-                                <div className="media-left">  
-                                    <Img fixed={data.propject1.childImageSharp.fixed} /> 
-                                </div>
-                                <div className="media-content">
-                                    <p className="title is-size-5-desktop is-size-6-touch">Gatsby Course</p>
-                                    <p className="subtitle is-5-desktop is-size-6-touch">@johnsmith</p>
-                                    <div className="tags">
-                                        <span className="tag">Gatsby</span>
-                                        <span className="tag">React</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-            </a>
-        </div> 
-    </div>
-
-    <div className="columns">
-        <div className="column is-half is-offset-one-quarter">
-            <a href="https://github.com/Seveshy/gitcompare">
-                <div className="card">
-                        <div className="card-content">
-                            <div className="media">
-                                <div className="media-left">  
-                                    <Img fixed={data.propject1.childImageSharp.fixed} /> 
-                                </div>
-                                <div className="media-content">
-                                    <p className="title is-size-5-desktop is-size-6-touch">Gatsby Course</p>
-                                    <p className="subtitle is-5-desktop is-size-6-touch">@johnsmith</p>
-                                    <div className="tags">
-                                        <span className="tag">Gatsby</span>
-                                        <span className="tag">React</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-            </a>
-        </div> 
-    </div>
-
-    <div className="columns">
-        <div className="column is-half is-offset-one-quarter">
-            <a href="https://github.com/Seveshy/gitcompare">
-                <div className="card">
-                        <div className="card-content">
-                            <div className="media">
-                                <div className="media-left">  
-                                    <Img fixed={data.propject1.childImageSharp.fixed} /> 
-                                </div>
-                                <div className="media-content">
-                                    <p className="title is-size-5-desktop is-size-6-touch">Gatsby Course</p>
-                                    <p className="subtitle is-5-desktop is-size-6-touch">@johnsmith</p>
-                                    <div className="tags">
-                                        <span className="tag">Gatsby</span>
-                                        <span className="tag">React</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-            </a>
-        </div> 
-    </div>
-
-    <div className="columns">
-        <div className="column is-half is-offset-one-quarter">
-            <a href="https://github.com/Seveshy/gitcompare">
-                <div className="card">
-                        <div className="card-content">
-                            <div className="media">
-                                <div className="media-left">  
-                                    <Img fixed={data.propject1.childImageSharp.fixed} /> 
-                                </div>
-                                <div className="media-content">
-                                    <p className="title is-size-5-desktop is-size-6-touch">Gatsby Course</p>
-                                    <p className="subtitle is-5-desktop is-size-6-touch">@johnsmith</p>
-                                    <div className="tags">
-                                        <span className="tag">Gatsby</span>
-                                        <span className="tag">React</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-            </a>
-        </div> 
-    </div>
-
-
-
-
-
-    <ul className="has-text-centered">
-            <li>
-                <a href="https://github.com/Seveshy/gitcompare">
-                Github Compare
-            </a>
-        </li>
-    </ul>
 </section>
-
 </Layout>
 );
 
